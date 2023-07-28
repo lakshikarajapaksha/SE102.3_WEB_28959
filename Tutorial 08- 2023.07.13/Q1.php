245f0da3c873136503ee66e18ab8e87ae96eefb2
lakshikarajapaksha <135578245+lakshikarajapaksha@users.noreply.github.com>
2023-07-28 22:38:58 +0530

    Add files via upload

---
+++ Tutorial 08- 2023.07.13/Q1.php
@@ -0,0 +1,28 @@
+
+<html>
+<head>
+<title>Hello</title>
+<script>
+var x=6;
+var y=4;
+z=x+y;
+document.write("<h1>The addition is: "+z);
+document.write("</h1><br>");
+z=x-y;
+document.write("<h1>The subsration is: "+z);
+document.write("</h1><br>");
+z=x*y;
+document.write("<h1>The Multiplication is: "+z);
+document.write("</h1><br>");
+z=x/y;
+document.write("<h1>The Division is: "+z);
+document.write("</h1><br>");
+</script>
+</head>
+<body>
+</body>
+</html>
+
+
+
+
